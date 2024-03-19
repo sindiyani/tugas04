e237f47dcc5d1bae26c67ae9158181fde58bd2cb
sindiyani <147934189+sindiyani@users.noreply.github.com>
2024-03-19 10:53:12 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 3.1.0
     </div>
-    <strong>Copyright &copy; 2023 <a href="https://adminlte.io">by Abdul Hamid</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2023 <a href="https://adminlte.io">by Sindyi Yani</a>.</strong> All rights reserved.
 </footer>
  
 <!-- Control Sidebar -->
